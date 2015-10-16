d0b0da91f5b5cb65fbf21360fb1003ffe8a63479
Iuri Madeira <iurimadeira@gmail.com>
2015-10-15 22:50:48 -0300

    Changed some things =D

--- button-pressed-led.rb
+++ button-pressed-led.rb
@@ -2,13 +2,18 @@ require 'artoo'
 
 connection :arduino, adaptor: :firmata, port: '/dev/ttyUSB0'
 
-device :led, driver: :led, pin: 13
-device :button, driver: :button, pin: 2, interval: 0.01
+device :led1, driver: :led, pin: 13
+device :led2, driver: :led, pin: 12
+device :button1, driver: :button, pin: 2, interval: 0.01
+device :button2, driver: :button, pin: 4, interval: 0.01
 
 work do
-  puts "Button activated LED [Iuri Madeira - github.com/iurimadeira]"
-  puts "Press the button connected on pin #{ button.pin }..."
+  puts "Two-Button activated LEDs [Iuri Madeira - github.com/iurimadeira]"
+  puts "Press the buttons to light the LEDs... =D"
 
-  on button, :push    => proc { led.on }
-  on button, :release => proc { led.off }
+  on button1, :push    => proc { led1.on }
+  on button1, :release => proc { led1.off }
+
+  on button2, :push    => proc { led2.on }
+  on button2, :release => proc { led2.off }
 end
